924ed8fd523bd0b8ba19e46c30101c7e2dcbeb76
prashant8khaitwada <prashantkhatiwada554@gmail.com>
2025-02-03 23:18:12 +0545

    Add Pug and Handlebars support, implement shop view with dynamic product rendering, and organize CSS files

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import adminRoutes from "./routes/admin.js";
-
 import shopRoutes from "./routes/shop.js";
 import __dirname from "./utils/path.js";
 
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+console.log("Serving static files from:", path.join(__dirname, "public"));
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
-import path from "path";
+// import path from "path";
 import express from "express";
-import __dirname from "../utils/path.js";
+// import __dirname from "../utils/path.js";
 import { products } from "./admin.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   console.log(products);
-  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  // res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  res.render("shop", { prods: products, docTitle: "My Shop" });
 });
 export default router;
