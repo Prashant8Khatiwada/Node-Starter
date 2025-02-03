ecb7f015f021026d619d0875ae8e8dd66a330b30
prashant8khaitwada <prashantkhatiwada554@gmail.com>
2025-02-04 00:21:03 +0545

    Refactor 404 error handling to use Pug templates, update product form to include price, description, and image URL, and enhance shop view with dynamic product rendering

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,24 @@
 import express from "express";
-import path from "path";
-import __dirname from "../utils/path.js";
+// import path from "path";
+// import __dirname from "../utils/path.js";
 
 const router = express.Router();
 router.use(express.urlencoded({ extended: false }));
 const products = [];
 // GET route to display the add-product form
 router.get("/add-product", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.render("add-product", { docTitle: "Add Product" });
 });
 
 // POST route to handle form submission
 router.post("/add-product", (req, res) => {
-  products.push({ title: req.body.title });
+  products.push({
+    title: req.body.title,
+    price: req.body.price,
+    description: req.body.description,
+    imageUrl: req.body.image,
+  });
   res.redirect("/");
 });
 
